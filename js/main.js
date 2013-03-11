c5ec6e1472e2d4cf3fdf1f1fb2bd01b364b8e329
Nikhil <nikhil.hclengg@gmail.com>
2013-03-11 09:16:37 +0530

    Check with alert

--- js/main.js
+++ js/main.js
@@ -14,10 +14,22 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+		var self=this;
+        this.store = new MemoryStore(function(){
+			self.showAlert('Store Initialized','Info');
+		});
 		this.sqlstore=new WebSqlStore();
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+		
+    },
+	
+	showAlert: function (message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
     }
+  }
 
 };
 
